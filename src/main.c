a54483c54f0d05ee7707fb03c0aa02732c543bc1
Saketh Sunil Pai <paisaketh@gmail.com>
2025-05-22 11:52:13 +0530

    my one desire

--- src/main.c
+++ src/main.c
@@ -71,7 +71,10 @@ int main(int argc, char **argv) {
     if (argc < 3) {
       printf("Usage: %s merge <branch>\n", argv[0]);
     } else {
-      merge_branch(repo, argv[2]);
+      int result = merge_branches(repo, argv[2]);
+      if (result == 1) {
+        printf("Resolve conflicts and commit the result\n");
+      }
     }
   } else if (strcmp(command, "stash") == 0) {
     if (argc < 3) {
